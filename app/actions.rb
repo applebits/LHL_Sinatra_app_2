be7d761d4abbb8a60a7d2fa148c8719bedca6e19
applebits <joeross.ca@gmail.com>
2016-01-25 21:04:33 +0000

    made first version of app

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,28 @@
 # Homepage (Root path)
-get '/' do
-  erb :index
+get '/songs' do
+  @songs = Song.all
+  erb:'songs/index'
+end
+
+get '/songs/new' do
+  @song = Song.new
+  erb :'songs/new'
+end
+
+get '/songs/:id' do
+  @song = Song.find params[:id]
+  erb :'songs/show'
+end
+
+post '/songs' do
+  @song = Song.new(
+    author: params[:author],
+    title: params[:title],
+    url: params[:url]
+  )
+  if @song.save
+    redirect '/songs'
+  else
+    erb :'songs/new'
+  end
 end
